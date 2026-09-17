a4ec3b1f51f156179a391a6c9b33eac5c43be3b5
agamjotsingh1 <agamjot.s.kohli@gmail.com>
2026-09-17 17:14:52 +0530

    added bwaves stats to pope_naive

--- experiments/pope_naive/jobfile.sh
+++ experiments/pope_naive/jobfile.sh
@@ -3,8 +3,8 @@
 #
 #
 # Traces:
-#    pope_naive_gemsfdtd_interleaved
-#    459.GemsFDTD-765B
+#    pope_naive_bwaves_interleaved
+#    603.bwaves_s-891B
 #
 #
 # Experiments:
@@ -14,7 +14,7 @@
 #
 #
 #
-/home/agam/Github/pope/Pythia//bin/perceptron-multi-multi-no-ship-1core --warmup_instructions=10000 --simulation_instructions=39990000 --config=/home/agam/Github/pope/Pythia//config/nopref.ini  -traces /home/agam/Github/pope/Pythia//traces/pope_naive_gemsfdtd_interleaved.champsimtrace.xz > pope_naive_gemsfdtd_interleaved_nopref.out 2>&1
-/home/agam/Github/pope/Pythia//bin/perceptron-multi-multi-no-ship-1core --warmup_instructions=10000 --simulation_instructions=39990000 --l2c_prefetcher_types=scooby --config=/home/agam/Github/pope/Pythia//config/pythia.ini  -traces /home/agam/Github/pope/Pythia//traces/pope_naive_gemsfdtd_interleaved.champsimtrace.xz > pope_naive_gemsfdtd_interleaved_pythia.out 2>&1
-/home/agam/Github/pope/Pythia//bin/perceptron-multi-multi-no-ship-1core --warmup_instructions=10000 --simulation_instructions=39990000 --config=/home/agam/Github/pope/Pythia//config/nopref.ini  -traces /home/agam/Github/pope/Pythia//traces/459.GemsFDTD-765B.champsimtrace.xz > 459.GemsFDTD-765B_nopref.out 2>&1
-/home/agam/Github/pope/Pythia//bin/perceptron-multi-multi-no-ship-1core --warmup_instructions=10000 --simulation_instructions=39990000 --l2c_prefetcher_types=scooby --config=/home/agam/Github/pope/Pythia//config/pythia.ini  -traces /home/agam/Github/pope/Pythia//traces/459.GemsFDTD-765B.champsimtrace.xz > 459.GemsFDTD-765B_pythia.out 2>&1
+/home/agam/Github/pope/Pythia//bin/perceptron-multi-multi-no-ship-1core --warmup_instructions=10000 --simulation_instructions=39990000 --config=/home/agam/Github/pope/Pythia//config/nopref.ini  -traces /home/agam/Github/pope/Pythia//traces/pope_naive_bwaves_interleaved.champsimtrace.xz > pope_naive_bwaves_interleaved_nopref.out 2>&1
+/home/agam/Github/pope/Pythia//bin/perceptron-multi-multi-no-ship-1core --warmup_instructions=10000 --simulation_instructions=39990000 --l2c_prefetcher_types=scooby --config=/home/agam/Github/pope/Pythia//config/pythia.ini  -traces /home/agam/Github/pope/Pythia//traces/pope_naive_bwaves_interleaved.champsimtrace.xz > pope_naive_bwaves_interleaved_pythia.out 2>&1
+/home/agam/Github/pope/Pythia//bin/perceptron-multi-multi-no-ship-1core --warmup_instructions=10000 --simulation_instructions=39990000 --config=/home/agam/Github/pope/Pythia//config/nopref.ini  -traces /home/agam/Github/pope/Pythia//traces/603.bwaves_s-891B.champsimtrace.xz > 603.bwaves_s-891B_nopref.out 2>&1
+/home/agam/Github/pope/Pythia//bin/perceptron-multi-multi-no-ship-1core --warmup_instructions=10000 --simulation_instructions=39990000 --l2c_prefetcher_types=scooby --config=/home/agam/Github/pope/Pythia//config/pythia.ini  -traces /home/agam/Github/pope/Pythia//traces/603.bwaves_s-891B.champsimtrace.xz > 603.bwaves_s-891B_pythia.out 2>&1
